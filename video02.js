279911c703615cdaa1454940ca385ba59a55428c
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-07 00:45:42 -0300

    Create video02.js

---
+++ video02.js
@@ -0,0 +1,41 @@
+function User(name){
+  this.name = name;
+}
+
+const user = new User('Mosh')
+
+//stack over flow:
+
+/*
+JavaScript 69.8%
+HTML       65.5%
+CSS        65.1%
+SQL        57.0%
+java       45.3%
+
+javascriptdeveloper 72000/yearon glassdor
+
+Front End
+Back End
+Full Stack
+
+What u can do with javascrip?
+-Web Mobbile Apps
+- REAL NEWORKS
+- GAMES
+- COMMANDS TO LINE
+
+Where javascript code ron?
+beowser - firefox, xhrome, javascript egine
+
+node -ECMAScript
+JavaScript - Programming Language
+
+
+
+*/
+console.log("Heelo World);
+           2+2
+            
+
+
